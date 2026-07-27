ff8bf510f34f10ab8fcd2a58505cb514eb571971
KEVIN01306 <kevin60sanchez31@gmail.com>
2026-07-27 12:21:04 -0600

    commit

--- src/i18n/config.i18n.ts
+++ src/i18n/config.i18n.ts
@@ -1,8 +1,6 @@
 export const LANGUAGES = {
     es: 'es',
     en: 'en',
-    fr: 'fr',
-    it: 'it',
 } as const;
 
 export type Lang = keyof typeof LANGUAGES;
--- src/i18n/ui.i18n.ts
+++ src/i18n/ui.i18n.ts
@@ -26,33 +26,5 @@ export const ui = {
         footer: {
             rights: '© 2024 Asyncronix. All rights reserved.',
         },
-    },
-    fr: {
-        nav: {
-            home: 'Accueil',
-            modules: 'Modules',
-            pricing: 'Prix',
-            about: 'À propos',
-            terms: 'Conditions',
-            privacy: 'Confidentialité',
-            login: 'Se connecter'
-        },
-        footer: {
-            rights: '© 2024 Asyncronix. Tous droits réservés.',
-        },
-    },
-    it: {
-        nav: {
-            home: 'Home',
-            modules: 'Moduli',
-            pricing: 'Prezzi',
-            about: 'Chi Siamo',
-            terms: 'Termini',
-            privacy: 'Privacy',
-            login: 'Accedi'
-        },
-        footer: {
-            rights: '© 2024 Asyncronix. Tutti i diritti riservati.',
-        },
     }
 } as const;
